1583448fef696aded2ed95db5c18d0800d4a4de9
Martin <69755205+armsamurai@users.noreply.github.com>
2022-02-11 18:59:32 +0300

    undo

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'django-insecure-8oko+=)3=9((m03f(&mbp2=4jc6gg(e8_11rpx4b==qe8pcg1('
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
